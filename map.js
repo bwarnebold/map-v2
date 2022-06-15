2a77ee99859632c7da02148d76bfcf9c55751f3f
Brett Warnebold <94931886+bwarnebold@users.noreply.github.com>
2022-06-15 13:13:50 -0400

    Add files via upload

--- map.js
+++ map.js
@@ -29,25 +29,32 @@ var viewbox = svg.getAttributeNS(null, "viewBox").split(" ");
 var centerX = parseFloat(viewbox[2]) / 2;
 var centerY = parseFloat(viewbox[3]) / 2;
 var matrixGroup = svg.getElementById("matrix-group");
+var isZoomed = false;
 
 function zoom(scale) {
+  if (isZoomed === false) {
     for (var i = 0; i < 4; i++) {
       transformMatrix[i] *= scale;
     }
     transformMatrix[4] += (1 - scale) * centerX;
     transformMatrix[5] += (1 - scale) * centerY;
-                  
+                
     var newMatrix = "matrix(" +  transformMatrix.join(' ') + ")";
     matrixGroup.setAttributeNS(null, "transform", newMatrix);
+    isZoomed = true;
   }
+}
 
-function pan(dx, dy) {     	
+function pan(dx, dy) {
+  if (isZoomed === false) {
     transformMatrix[4] += dx;
     transformMatrix[5] += dy;
               
     var newMatrix = "matrix(" +  transformMatrix.join(' ') + ")";
     matrixGroup.setAttributeNS(null, "transform", newMatrix);
+    isZoomed = true;
   }
+}
 
 // Zoom + Pan to State End //
 
